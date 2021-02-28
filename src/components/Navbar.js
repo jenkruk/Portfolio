4d6fb069698fb9797c0523f50a5bb4fc212ac4e5
Jennifer Kruk <jenkruk@gmail.com>
2021-02-28 12:49:23 -0700

    navbar reveals on scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-scroll';
 //  REACT FONTAWESOME IMPORTS
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 const Navbar = () => {
+
+    const [navbar, setNavbar] = useState(false);
+
+    const revealNav = () => {
+        if (window.scrollY >= 1060) {
+            setNavbar(true)
+        } else {
+            setNavbar(false);
+        }
+    }
+
+    window.addEventListener("scroll", revealNav)
+
     return (
-    <nav className="navbar navbar-expand-lg fixed-top">
+    <nav className={navbar ? 'navbar active navbar-expand-lg fixed-top' : 'navbar navbar-expand-lg fixed-top'}>
         <div className="container">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <FontAwesomeIcon icon={faBars} style={{ color: "#fff"}}/>
