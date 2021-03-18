241876dcccdff4b5c978ecb9daf862fcaaf89117
Jennifer Kruk <jenkruk@gmail.com>
2021-03-18 13:48:18 -0700

    used css grid to make footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,58 +5,41 @@ import { Link } from 'react-scroll';
 
 const Footer = () => {
     return (
-        <div className="footer pb-5 text-nowrap">
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-4">
-                        <div className="row h-100">
-                            <div className="col">
-                                <p className="d-flex w-100">
-                                    Jennifer Kruk
-                                </p>
-                                <div className="d-flex w-100">
-                                    <a href="mailto: jenkruk@gmail.com" rel="noreferrer" target="_blank">contactkruk@gmail.com</a>
-                                </div>
-                                <div className="d-flex w-100">
-                                    <p>Mesa, AZ</p>
-                                </div>
-                            </div>
-                        </div>
+        <div className="footer">
+            <div className="infoRow">
+                    <p className="name">
+                        Jennifer Kruk
+                    </p>
+                    <div className="email">
+                        <a href="mailto: jenkruk@gmail.com" rel="noreferrer" target="_blank">contactkruk@gmail.com</a>
                     </div>
-                    <div className="col-sm-4 align-items-center">
-                        <div className="d-flex justify-content-center mb-3">
-                            <a href="https://github.com/jenkruk" rel="noreferrer" target="_blank">
-                                <div><FontAwesomeIcon className="footerIcon github" icon={faGithubSquare} size="2x" /></div>
-                            </a>
-                            <a href="https://www.linkedin.com/in/jenkruk/" rel="noreferrer" target="_blank">
-                                <div><FontAwesomeIcon className="footerIcon linkedin" icon={faLinkedin} size="2x" /></div>
-                            </a>
-                        </div>
-                        <div className="row d-block text-center">
-                        <span className="m-auto copyright">
-                            Copyright &copy;{new Date().getFullYear()}
-                        </span>
-                        <span className="m-auto d-block">
-                            All Rights Reserved
-                        </span>
+                    <div className="town">
+                        <p>Mesa, AZ</p>
                     </div>
-                </div>
-                <div className="col-sm-4">
-                        <div className="row h-100">
-                            <div className="col text-right">
-                                <Link smooth={true} to="about" className="" href="#">About</Link>
-                                <br/>
-                                <Link smooth={true} to="skills" className="" href="#">Skills</Link>
-                                <br/>
-                                <Link smooth={true} to="experience" className="" href="#">Experience</Link>
-                                <br/>
-                                <Link smooth={true} to="portfolio" className="" href="#">Portfolio</Link>
-                            </div>
-                        </div>
-                    </div> 
-            </div> 
-        </div> 
-        </div> 
+            </div>
+            <div className="github">
+                <a href="https://github.com/jenkruk" rel="noreferrer" target="_blank">
+                    <div><FontAwesomeIcon icon={faGithubSquare} size="2x" /></div>
+                </a>
+            </div>
+            <div className="linkedin">
+                <a href="https://www.linkedin.com/in/jenkruk/" rel="noreferrer" target="_blank">
+                    <div><FontAwesomeIcon icon={faLinkedin} size="2x" /></div>
+                </a>
+            </div>
+            <div className="linksRow">
+                    <Link smooth={true} to="about" href="#">About</Link>
+                    <br/>
+                    <Link smooth={true} to="skills" href="#">Skills</Link>
+                    <br/>
+                    <Link smooth={true} to="experience" href="#">Experience</Link>
+                    <br/>
+                    <Link smooth={true} to="portfolio" href="#">Portfolio</Link>
+            </div>
+            <div className="copyright">
+                Copyright &copy;{new Date().getFullYear()} | All Rights Reserved
+            </div>
+        </div>
     )
 }
 
