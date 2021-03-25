032305211f76d38e24beb5d337bd974e1926a374
Jennifer Kruk <jenkruk@gmail.com>
2021-03-25 09:29:47 -0700

    completed mobile menu collapse functionality and added references section to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,23 @@ const Navbar = () => {
     const [navbar, setNavbar] = useState(false);
     const [open, setOpen] = useState(false);
 
+    const handleClick = (e) => {
+        e.preventDefault();
+        const hideMenu = document.getElementById("navbarSupportedContent");
+        hideMenu.classList.remove("show");
+        const handleToggler = document.getElementById("toggler");
+        handleToggler.classList.add("collapsed");
+    }
+
+    const handleState = (e) => {
+        e.preventDefault();
+        if (open) {
+            setOpen(false)
+        } else {
+            setOpen(true)
+        }
+    }
+
     const revealNav = () => {
         if (window.scrollY >= 400) {
             setNavbar(true)
@@ -23,29 +40,29 @@ const Navbar = () => {
     <nav className={navbar ? 'navbar active navbar-expand-lg fixed-top' : 'navbar navbar-expand-lg fixed-top'} >
         {/* <div className="container"> */}
             <div className="navbar-brand pl-5" href="#">Jennifer Kruk</div>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={() => setOpen(open ? false : "open")}>
+                <button className="navbar-toggler" id="toggler"  onClick={handleState} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <FontAwesomeIcon icon={faBars} style={{ color: "#fff"}} />
                 </button>
             {/* </div> */}
-            <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{display: open ? 'block' : 'none'}}>
+            <div className="navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item active">
-                        <Link smooth={true} to="home" offset={-100} className="nav-link" href="#" onClick={() => {setOpen(false)}}>⇧top<span className="sr-only">(current)</span></Link>
+                        <Link smooth={true} to="home" offset={-100} className="nav-link" href="#" onClick={handleClick}>⇧top<span className="sr-only">(current)</span></Link>
                     </li>
                     <li className="nav-item">
-                        <Link smooth={true} to="about" offset={-100} className="nav-link" href="#" onClick={() => {setOpen(false)}}>about</Link>
+                        <Link smooth={true} to="about" offset={-100} className="nav-link" href="#" onClick={handleClick}>about</Link>
                     </li>
                     <li className="nav-item">
-                        <Link smooth={true} to="skills" offset={-90} className="nav-link" href="#" onClick={() => {setOpen(false)}}>skills</Link>
+                        <Link smooth={true} to="skills" offset={-90} className="nav-link" href="#" onClick={handleClick}>skills</Link>
                     </li>
                     <li className="nav-item" >
-                        <Link smooth={true} to="experience" offset={-100} className="nav-link" href="#" onClick={() => {setOpen(false)}}>experience</Link>
+                        <Link smooth={true} to="experience" offset={-100} className="nav-link" href="#" onClick={handleClick}>experience</Link>
                     </li>
                     <li className="nav-item">
-                        <Link smooth={true} to="portfolio" offset={-100} className="nav-link" href="#" onClick={() => {setOpen(false)}}>portfolio</Link>
+                        <Link smooth={true} to="portfolio" offset={-100} className="nav-link" href="#" onClick={handleClick}>portfolio</Link>
                     </li>
                     <li className="nav-item">
-                        <Link smooth={true} to="contact" offset={-100} className="nav-link" href="#" onClick={() => {setOpen(false)}}>contact</Link>
+                        <Link smooth={true} to="contact" offset={-100} className="nav-link" href="#" onClick={handleClick}>contact</Link>
                     </li>
                 </ul>
             </div>
